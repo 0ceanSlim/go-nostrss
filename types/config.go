df75d27ba005f31d65fb55666d1448e7522eed13
Cow <cowingtonpost@gmail.com>
2025-07-23 17:12:50 -0700

    allow customizing message format in config.yml

--- types/config.go
+++ types/config.go
@@ -2,10 +2,12 @@ package types
 
 // Config represents the YAML configuration structure
 type Config struct {
-	RSSFeed            string `yaml:"rss_feed"`
-	NostrPrivateKey    string `yaml:"nostr_private_key"`
-	NostrPublicKey     string `yaml:"nostr_public_key"` // Added public key
-	RelayURL           string `yaml:"relay_url"`
-	FetchIntervalMins  int    `yaml:"fetch_interval_minutes"`
-	CacheFile          string `yaml:"cache_file"`
+	RSSFeed           string `yaml:"rss_feed"`
+	NostrPrivateKey   string `yaml:"nostr_private_key"`
+	NostrPublicKey    string `yaml:"nostr_public_key"` // Added public key
+	RelayURL          string `yaml:"relay_url"`
+	FetchIntervalMins int    `yaml:"fetch_interval_minutes"`
+	CacheFile         string `yaml:"cache_file"`
+	MessageFormat     string `yaml:"message_format"`
 }
+
